ac10179106bb9346758675ef5be27b594b667dea
Edward Waked <ebwaked@hotmail.com>
2018-06-29 01:16:20 -0400

    app running on live server

--- front-end/app/src/app.js
+++ front-end/app/src/app.js
@@ -8,14 +8,14 @@ import 'angular-material';
 import AppController from 'src/AppController';
 import Users from 'src/users/Users';
 
-export default angular.module( 'nextech-interview-app', [ 'ngMaterial', Users.name ] )
-  .config(($mdIconProvider, $mdThemingProvider) => {
+export default angular.module('nextech-interview-app', ['ngMaterial', Users.name])
+    .config(($mdIconProvider, $mdThemingProvider) => {
 
-    $mdIconProvider
-      .icon("menu", "./assets/svg/menu.svg", 24)
+        $mdIconProvider
+            .icon("menu", "./assets/svg/menu.svg", 24)
 
-    $mdThemingProvider.theme('default')
-      .primaryPalette('teal')
-      .accentPalette('green');
-  })
-  .controller('AppController', AppController);
+        $mdThemingProvider.theme('default')
+            .primaryPalette('teal')
+            .accentPalette('green');
+    })
+    .controller('AppController', AppController);
--- front-end/app/src/boot/boot.js
+++ front-end/app/src/boot/boot.js
@@ -1,4 +1,4 @@
-import angular from 'angular';
+//import angular from 'angular';
 
 import App from 'src/app';
 
@@ -7,14 +7,14 @@ import App from 'src/app';
  * the application classes have been loaded.
  */
 angular
-  .element( document )
-  .ready( function() {
-    angular
-      .module( 'nextech-interview-app-bootstrap', [ App.name ] )
-      .run(()=>{
-        console.log(`Running the 'nextech-interview-app-bootstrap'`);
-      });
+    .element(document)
+    .ready(function() {
+        angular
+            .module('nextech-interview-app-bootstrap', [App.name])
+            .run(() => {
+                console.log(`Running the 'nextech-interview-app-bootstrap'`);
+            });
 
-    let body = document.getElementsByTagName("body")[0];
-    angular.bootstrap( body, [ 'nextech-interview-app-bootstrap' ]);
-  });
+        let body = document.getElementsByTagName("body")[0];
+        angular.bootstrap(body, ['nextech-interview-app-bootstrap']);
+    });
