25cca6a43624b18a228fcf5b4bb36092924a80c7
Edward Waked <ebwaked@hotmail.com>
2018-07-04 18:42:26 -0400

    more changes to 2.1

--- front-end/app/src/app.js
+++ front-end/app/src/app.js
@@ -1,5 +1,6 @@
 // Load libraries
 import angular from 'angular';
+//import { http } from 'angular/http';
 
 import 'angular-animate';
 import 'angular-aria';
@@ -7,6 +8,7 @@ import 'angular-material';
 
 import AppController from 'src/AppController';
 import Users from 'src/users/Users';
+//import HttpClientModule from 'angular/common/http';
 
 export default angular.module('nextech-interview-app', ['ngMaterial', Users.name])
     .config(($mdIconProvider, $mdThemingProvider) => {
